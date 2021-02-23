769094986768e72a34072a0ade0dcec46d377740
markcfong561 <35080862+markcfong561@users.noreply.github.com>
2021-02-23 16:52:20 -0500

    Fixed Blinky Lights
    By: @Tazmania40

--- RobotCode/src/main/java/frc/robot/Constants.java
+++ RobotCode/src/main/java/frc/robot/Constants.java
@@ -1,7 +1,6 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.util.Color;
-import com.revrobotics.ColorMatch;
 import edu.wpi.first.wpilibj.Joystick;
 import frc.lib.util.HIDHelper;
 
@@ -168,12 +167,7 @@ public class Constants {
     public static final double COLOR_WHEEL_KP = 0.0;
     public static final double COLOR_WHEEL_KI = 0.0;
     public static final double COLOR_WHEEL_KD = 0.0;
-    public static final double COLOR_WHEEL_VCOMP = 0.0;
-
-    public static final Color kBlueTarget = ColorMatch.makeColor(0.128, 0.413, 0.459);
-    public static final Color kGreenTarget = ColorMatch.makeColor(0.172, 0.564, 0.264);
-    public static final Color kRedTarget = ColorMatch.makeColor(0.498, 0.352, 0.150);
-    public static final Color kYellowTarget = ColorMatch.makeColor(0.315, 0.553, 0.132);    
+    public static final double COLOR_WHEEL_VCOMP = 0.0;    
 
     //Limelight Constants
     public static final double LIMELIGHT_DEG_FOV = 0.0; //TODO CALCULATE FOV
--- RobotCode/src/main/java/frc/robot/subsystems/Lights.java
+++ RobotCode/src/main/java/frc/robot/subsystems/Lights.java
@@ -7,18 +7,19 @@ import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
-import frc.lib.statemachine.StateMachine;
+import frc.lib.loops.ILooper;
+import frc.lib.loops.Loop;
 import frc.robot.Constants;
 
 public class Lights extends Subsystem {
     private AddressableLED mled;
     private AddressableLEDBuffer mLEDBuffer;
     private int numberOfBalls;
-    private boolean targeted, uprightsUp, intakeDown, climbUp, FMSOn;
+    private boolean targeted, intakeDown, enabled, FMSOn;
     private Color allianceColor, colorWheelColor;
     private int state = 0;
     private lightModes currentLightMode = lightModes.beforeStart;
-    private Value intakeState;
+    private Color testColor;
 
     private Lights() {
         mled = new AddressableLED(Constants.LED_PORT);
@@ -36,134 +37,99 @@ public class Lights extends Subsystem {
     public void readPeriodicInputs() {
         // intakeState = Superstructure.getInstance().intakeDown();
         FMSOn = DriverStation.getInstance().isFMSAttached();
-        if (intakeState == Value.kForward) {
+        enabled = DriverStation.getInstance().isEnabled();
+        numberOfBalls = Superstructure.getInstance().getNumberOfBalls();
+        targeted = Shooter.getInstance().onTarget();
+        
+        // Testing
+        //currentLightMode = lightModes.targeting;
+        //targeted = SmartDashboard.getBoolean("Lights/targetedTest", true);
+        //testColor = Color.kBlue;
+        //numberOfBalls = 5;
+
+        /*if (intakeState == Value.kForward) {
             intakeDown = true;
         } else {
             intakeDown = false;
-        }
-        if (FMSOn = true) {
-            currentLightMode = lightModes.allianceColor;
-            if (DriverStation.getInstance().getAlliance() == Alliance.Blue) {
-                allianceColor = Color.kFirstBlue;
-            } else if (DriverStation.getInstance().getAlliance() == Alliance.Red) {
-                allianceColor = Color.kDarkRed;
+        }*/
+        
+        if (enabled) {
+            if (numberOfBalls == -2 || numberOfBalls == 5) {
+                currentLightMode = lightModes.targeting;
             } else {
-                allianceColor = Color.kChocolate;
+                currentLightMode = lightModes.indexNum;
             }
         } else {
-            currentLightMode = lightModes.beforeStart;
-        }
-        // TODO Implement targeting
-        uprightsUp = Climber.getInstance().getUnfolded();
-        climbUp = Climber.getInstance().getClimbed();
-        /*if (uprightsUp && !climbUp) {
-            currentLightMode = lightModes.colorWheel;
-            switch (ColorWheel.getInstance().cDetected()) {
-            case 'U':
-                colorWheelColor = Color.kBlack;
+            if (FMSOn == true) {
+                currentLightMode = lightModes.allianceColor;
+                if (DriverStation.getInstance().getAlliance() == Alliance.Blue) {
+                    allianceColor = Color.kFirstBlue;
+                } else if (DriverStation.getInstance().getAlliance() == Alliance.Red) {
+                    allianceColor = Color.kDarkRed;
+                } else {
+                    allianceColor = Color.kChocolate;
+                } 
+            } else {
+                currentLightMode = lightModes.beforeStart;
+            }
+        } 
+
+        //Assigning the  LEDs
+        switch (currentLightMode) {
+            case Testing:
+                for (var i = 0; i < (int)(mLEDBuffer.getLength() * (state / 7)); i++) {
+                mLEDBuffer.setLED(i, testColor);
+            }
+            case targeting:
+                if (targeted) {
+                    for (var i = 0; i < mLEDBuffer.getLength(); i++) {
+                        mLEDBuffer.setRGB(i, 0, 150, 0);
+                    }
+                } else {
+                    for (var i = 0; i < mLEDBuffer.getLength(); i++) {
+                        mLEDBuffer.setRGB(i, 150, 0, 0);
+                    }
+                }
+                // System.out.println("Lights Set");
                 break;
-            case 'R':
-                colorWheelColor = Color.kRed;
+            case indexNum:
+                if (numberOfBalls >= 0 && numberOfBalls <= 4) {
+                    for (var i = 0; i < mLEDBuffer.getLength(); i++) {
+                        mLEDBuffer.setLED(i, Color.kBlack);
+                    }
+                    for (var i = 0; i < (mLEDBuffer.getLength() * (.2 * numberOfBalls)); i++) {
+                        mLEDBuffer.setLED(i, Color.kYellow);
+                    }
+                } else {
+                    for (var i = 0; i < (mLEDBuffer.getLength() * (.2 * numberOfBalls)); i++) {
+                        mLEDBuffer.setHSV(i, i * (239 / mLEDBuffer.getLength()), 226, 62);
+                    }
+                }
+                // System.out.println("Lights Set");
                 break;
-            case 'G':
-                colorWheelColor = Color.kDarkGreen;
+            case allianceColor:
+                for (var i = 0; i < mLEDBuffer.getLength(); i++) {
+                    mLEDBuffer.setLED(i, allianceColor);
+                }
+                // System.out.println("Lights Set");
                 break;
-            case 'Y':
-                colorWheelColor = Color.kGold;
+            case beforeStart:
+                for (var i = 0; i < (mLEDBuffer.getLength() * (.2 * numberOfBalls)); i++) {
+                    mLEDBuffer.setHSV(i, i * ((239 / mLEDBuffer.getLength())), 150, 75);
+                }
                 break;
-            case 'B':
-                colorWheelColor = Color.kBlue;
+            default:
+                for (var i = 0; i < mLEDBuffer.getLength(); i++) {
+                    mLEDBuffer.setLED(i, Color.kPurple);
+                }
+                // System.out.println("Lights Set");
                 break;
             }
-        } else {*/
-            currentLightMode = lightModes.targeting;
-        //}
-        // Testing
-        // currentLightMode = lightModes.Testing;
-        // colorWheelColor = Color.kBlue;
-        // numberOfBalls = 5;
     }
 
     @Override
     public void writePeriodicOutputs() {
-        switch (currentLightMode) {
-        case Testing:
-            for (var i = 0; i < (int)(mLEDBuffer.getLength() * (state / 7)); i++) {
-            mLEDBuffer.setLED(i, Color.kBlue);
-        }
-        case colorWheel:
-            for (var i = 0; i < mLEDBuffer.getLength(); i++) {
-                // Sets the specified LED to the RGB values for red
-                mLEDBuffer.setLED(i, colorWheelColor);
-                // System.out.println("Lights Set");
-            }
-            break;
-        case targeting:
-            if (Shooter.getInstance().onTarget()) {
-                for (var i = 0; i < mLEDBuffer.getLength(); i++) {
-                    mLEDBuffer.setRGB(i, 0, 150, 0);
-                }
-            } else {
-                for (var i = 0; i < mLEDBuffer.getLength(); i++) {
-                    mLEDBuffer.setRGB(i, 150, 0, 0);
-                }
-            }
-            // System.out.println("Lights Set");
-            break;
-        case indexNum:
-            if (numberOfBalls >= 1 && numberOfBalls <= 4) {
-                for (var i = 0; i < (mLEDBuffer.getLength() * (.2 * numberOfBalls)); i++) {
-                    mLEDBuffer.setLED(i, Color.kYellow);
-                }
-            } else {
-                for (var i = 0; i < (mLEDBuffer.getLength() * (.2 * numberOfBalls)); i++) {
-                    mLEDBuffer.setHSV(i, i * (239 / mLEDBuffer.getLength()), 226, 62);
-                }
-            }
-            // System.out.println("Lights Set");
-            break;
-        case allianceColor:
-            for (var i = 0; i < mLEDBuffer.getLength(); i++) {
-                mLEDBuffer.setLED(i, allianceColor);
-            }
-            // System.out.println("Lights Set");
-            break;
-        case beforeStart:
-            for (var i = 0; i < (mLEDBuffer.getLength() * (.2 * numberOfBalls)); i++) {
-                mLEDBuffer.setHSV(i, i * ((239 / mLEDBuffer.getLength())), 226, 62);
-            }
-            break;
-        default:
-            for (var i = 0; i < mLEDBuffer.getLength(); i++) {
-                mLEDBuffer.setLED(i, Color.kPurple);
-            }
-            // System.out.println("Lights Set");
-            break;
-        }
         mled.setData(mLEDBuffer);
-    }
-
-    /*
-     * This converts the char inputted into a hue value.
-     * 
-     * @param color color RGBY as a char, capitals only
-     * 
-     * @return hue value of the color inputted
-     */
-    public int interpretColor(char color) {
-        switch (color) {
-        case 'R':
-            return 0;
-        case 'Y':
-            return 60;
-        case 'G':
-            return 120;
-        case 'B':
-            return 180;
-        default:
-            return 300;
-
-        }
     }
 
     public void testLights(int state)
@@ -174,7 +140,7 @@ public class Lights extends Subsystem {
 
     @Override
     public void reset() {
-        mLEDBuffer = new AddressableLEDBuffer(60);
+        mLEDBuffer = new AddressableLEDBuffer(Constants.LED_LENGTH);
         mled.setLength(mLEDBuffer.getLength());
         mled.setData(mLEDBuffer);
         mled.start();
@@ -185,10 +151,11 @@ public class Lights extends Subsystem {
     @Override
     public void outputTelemetry() {
         SmartDashboard.putNumber("Lights/Length of String", mLEDBuffer.getLength());
+        SmartDashboard.putString("Lights/LightMode", currentLightMode.toString());
     }
 
     enum lightModes {
-        targeting, colorWheel, indexNum, allianceColor, beforeStart, Testing;
+        targeting, indexNum, allianceColor, beforeStart, Testing;
     }
 
 }
